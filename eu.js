9882f8f00d6a6fe589dc26a7fbdbc4b2d4bd1ce5
rianymello <mello.riany@gmail.com>
2022-03-30 21:36:11 -0300

    tentativa de animação

---
+++ eu.js
@@ -0,0 +1,42 @@
+var div = document.getElementById('log');
+var textos = ['Olá, saudações!', 'Tudo bem?!',
+ 'Meu nome é Riany', 'Estou estudado programação',
+  'Espero que isso aqui dê certo...'];
+
+function escrever(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 700); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 110);
+}
+
+function limpar(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 97);
+}
+
+function rodape(conteudos, el) {
+    var atual = -1;
+	function prox(cb){
+		if (atual < conteudos.length - 1) atual++;
+		else atual = 0;
+		var str = conteudos[atual];
+		escrever(str, function(){
+			limpar(prox);
+		});
+	}
+	prox(prox);
+}
+rodape(textos);
